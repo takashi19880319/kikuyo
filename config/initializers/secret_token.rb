e6b4ed98bec096629c16c5d16604c9e695722ac8
Takashi Hashiguchi <takashi19880319@gmail.com>
2015-10-09 16:35:10 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kikuyo::Application.config.secret_key_base = 'f9800b366caca43d656218a1f416fa3f21f53d5d99563028b072b09930c11ea3acb295a9a1aba5356dcf06038991ce185dcb2e1d10e8c4bc67db6b1e4331c6e3'
